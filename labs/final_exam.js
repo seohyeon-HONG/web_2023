3b5abb09ba2405b43826f173d75187c6d425bdf1
seohyeon-HONG <hsally1207@naver.com>
2023-12-21 20:11:04 +0900

    index 페이지 생성

---
+++ labs/final_exam.js
@@ -0,0 +1,80 @@
+// 한 줄: 아아아 //
+/* 여러 줄:
+ 아아 */
+
+// 내장 자료형: boolean - true,false/ undefined/ null / number / string / symbol //
+
+/* object types : 객체 자료형
+
+account{
+    owner : "홍길동",
+    num : "110-1234-12",
+    balance : 350,
+
+    deposit : function() {...},
+    withdraw : function() {...},
+    inquiry : function() {...}
+};
+
+ */
+
+/* 변수
+var
+
+var score;
+var year, month, day;
+var address = "서울시";
+
+var x, z;
+function f(){
+    var y;
+    x = 10;
+    y = 10;
+    z = 10;
+}
+
+var x; // 전역변수 x 선언 //
+function f(){
+    var x;
+    x = 1;
+    this.x = 100;
+}
+ */
+
+// 연산자와 연산식 //
+var col = [“red”, “blue”, “yellow”, “green”] ;
+v)ar cols = “” ;
+for (var i=0; i<col.length; i++) {
+    if (col[i]<“purple”) {
+cols += col[i]
+    }
+}
+
+console.log(cols
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
